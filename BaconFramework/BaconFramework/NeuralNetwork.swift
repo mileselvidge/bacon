c6dbbe5395ebc349eb91c37a1daaaa747674d28b
Shaan Singh <shaansingh@bluecocoa.com>
2017-04-26 16:08:04 -0400

    Cost calculation for convergence monitoring. Demo now automates sample data generation.

--- BaconFramework/BaconFramework/NeuralNetwork.swift
+++ BaconFramework/BaconFramework/NeuralNetwork.swift
@@ -28,11 +28,11 @@ public class NeuralNetwork {
     }
     
     /**
-     Trains the neural network after initialization.
+     Trains the neural network after initialization. Returns the hidden layer matrix and weights matrix flattened as arrays.
      - parameter input: Each row in the input matrix corresponds to one sample. The number of columns is the number of input nodes.
      - parameter output: Like the input matrix, each row corresponds to one sample. The number of columns is the number of output nodes.
      */
-    public func train(_ input: [[Double]], _ output: [[Double]]) {
+    public func train(_ input: [[Double]], _ output: [[Double]]) -> (hiddenLayer: [Double], weights: [Double])? {
         
         // Seed
         srand48(1)
@@ -53,44 +53,48 @@ public class NeuralNetwork {
         
         for iteration in 1...iterations {
             
-            if verbose {
-                print("Iteration \(iteration)")
-            }
-            
             let layer0 = input
             
             // Forward prop step 1: layer0 * synapse
-            var dotted = [Double](repeating: 0, count: layer0.count * synapse[0].count)
-            cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, Int32(layer0.count), Int32(synapse[0].count), Int32(layer0[0].count), 1.0, Array(layer0.joined()), Int32(layer0[0].count), Array(synapse.joined()), Int32(synapse[0].count), 0.0, &(dotted), Int32(synapse[0].count))
+            var multiplied = [Double](repeating: 0, count: layer0.count * synapse[0].count)
+            cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, Int32(layer0.count), Int32(synapse[0].count), Int32(layer0[0].count), 1.0, Array(layer0.joined()), Int32(layer0[0].count), Array(synapse.joined()), Int32(synapse[0].count), 0.0, &(multiplied), Int32(synapse[0].count))
             
             // Forward prop step 2: activation function
-            let layer1 = calculate(dotted, with: .sigmoid)
+            let layer1 = calculate(multiplied, with: .sigmoid)
             
             // Make layer1 negative
             var negLayer1 = layer1
             vDSP_vnegD(layer1, 1, &(negLayer1), 1, vDSP_Length(negLayer1.count))
             
-            // Layer1 error = output - layer1
-            var layer1Error = negLayer1
-            cblas_daxpy(Int32(Array(output.joined()).count), 1.0, Array(output.joined()), 1, &(layer1Error), 1)
+            // Layer1 cost derivative = output - layer1
+            var layer1CostDerivative = negLayer1
+            cblas_daxpy(Int32(Array(output.joined()).count), 1.0, Array(output.joined()), 1, &(layer1CostDerivative), 1)
+            
+            // cost derivative.T
+            var l1CostDerivTransposed = [Double](repeating: 0, count: output.count * output[0].count)
+            vDSP_mtransD(layer1CostDerivative, 1, &(l1CostDerivTransposed), 1, vDSP_Length(output[0].count), vDSP_Length(output.count))
+            
+            // Layer1 cost = cost derivative.T * cost derivative
+            var layer1Cost = [Double](repeating: 0, count: output[0].count)
+            cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, Int32(output[0].count), Int32(output[0].count), Int32(output.count), 1.0, l1CostDerivTransposed, Int32(output.count), layer1CostDerivative, Int32(output[0].count), 0.0, &(layer1Cost), Int32(output[0].count))
             
             // Get derivative of sigmoid
             let derivative = calculateDerivative(layer1, with: .sigmoid)
             
-            // Error weighted derivative = error * derivative of sigmoid (element-wise multiplication)
-            var errorWeightedDerivative = [Double](repeating: 0, count: layer1Error.count)
-            vDSP_vmulD(layer1Error, 1, derivative, 1, &errorWeightedDerivative, 1, vDSP_Length(layer1Error.count))
+            // Error weighted derivative = cost derivative * derivative of sigmoid (element-wise multiplication)
+            var errorWeightedDerivative = [Double](repeating: 0, count: layer1CostDerivative.count)
+            vDSP_vmulD(layer1CostDerivative, 1, derivative, 1, &errorWeightedDerivative, 1, vDSP_Length(layer1CostDerivative.count))
             
             // Transpose layer0
             var transposed = [Double](repeating: 0, count: Array(layer0.joined()).count)
             vDSP_mtransD(Array(layer0.joined()), 1, &(transposed), 1, vDSP_Length(layer0[0].count), vDSP_Length(layer0.count))
             
             // Compute layer0.T * errorWeightedDerivative
-            var dottedFinal = [Double](repeating: 0, count: layer0[0].count * output[0].count)
-            cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, Int32(layer0[0].count), Int32(output[0].count), Int32(layer0.count), 1.0, transposed, Int32(layer0.count), errorWeightedDerivative, Int32(output[0].count), 0.0, &(dottedFinal), Int32(output[0].count))
+            var multipliedFinal = [Double](repeating: 0, count: layer0[0].count * output[0].count)
+            cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, Int32(layer0[0].count), Int32(output[0].count), Int32(layer0.count), 1.0, transposed, Int32(layer0.count), errorWeightedDerivative, Int32(output[0].count), 0.0, &(multipliedFinal), Int32(output[0].count))
             
             // Adjust the weights
-            var results = dottedFinal
+            var results = multipliedFinal
             cblas_daxpy(Int32(Array(synapse.joined()).count), 1.0, Array(synapse.joined()), 1, &(results), 1)
             
             // Map results back to synapse's inherent structure
@@ -100,17 +104,25 @@ public class NeuralNetwork {
                 })
             })
             
+            if verbose {
+                print("Iteration \(iteration), cost = \(layer1Cost[0])")
+            }
+            
             if iteration == iterations {
                 print("Training complete!\n")
                 
                 print("Hidden layer:")
-                print("\(layer1)\n")
+                print(layer1)
                 
-                print("Weights:")
+                print("\nWeights:")
                 print(Array(synapse.joined()))
+                
+                return (layer1, Array(synapse.joined()))
             }
             
         }
+        
+        return nil
     }
     
 }
--- BaconSampleProject/BaconSampleProject/ViewController.swift
+++ BaconSampleProject/BaconSampleProject/ViewController.swift
@@ -10,47 +10,77 @@ import Cocoa
 import BaconFramework
 
 class ViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
-        /* **** The pattern here is: if either column 1 or 2 of the input is 1, then the output is 1. However, if column 1 and 2 are both 1 or both 0, then the output is 0. Column 3 is irrelevant. If you run this neural network with ActivationFunction.sigmoid and 100,000 iterations, the hidden layer results should be quite accurate. Below is the results of one of our tests:
-            
-            Hidden layer:
-            [0.99495044083462603, 2.1832008463678404e-05, 0.005533223089738218, 0.0039085393430607846, 0.99997176240828434]
+        /* **** The pattern here is: if either column 1 or 2 of the input is 1, then the output is 1. However, if column 1 and 2 are both 1 or both 0, then the output is 0. Column 3 is irrelevant. Below are the results and configuration of one of our tests:
          
-            Weights:
-            [10.47483834281852, -16.015518754305521, -5.1914412862863717]
-        **** */
+         input:
+         [[1, 0, 1],
+         [1, 1, 1],
+         [0, 0, 1],
+         [1, 1, 0],
+         [1, 0, 0]]
+         
+         output:
+         [[1],
+         [0],
+         [0],
+         [0],
+         [1]]
+         
+         Hidden layer:
+         [0.99495044083462603, 2.1832008463678404e-05, 0.005533223089738218, 0.0039085393430607846, 0.99997176240828434]
+         
+         Weights:
+         [10.47483834281852, -16.015518754305521, -5.1914412862863717]
+         **** */
         
-        let input: [[Double]] = [
-            [1, 0, 1],
-            [1, 1, 1],
-            [0, 0, 1],
-            [1, 1, 0],
-            [1, 0, 0]
-        ]
+        // M = how many samples you want
+        let M = 1000
         
-        let output: [[Double]] = [
-            [1],
-            [0],
-            [0],
-            [0],
-            [1]
-        ]
+        // Creates a M x 3 input matrix with random zeros and ones
+        var input = [[Double]](repeating: [Double](repeating: 0, count: 3), count: M)
+        input = input.map {
+            $0.map {
+                $0 + Double(arc4random_uniform(2))
+            }
+        }
         
-        let neuralNetwork = NeuralNetwork(activationFunction: .sigmoid, iterations: 100000, verbose: true)
-        neuralNetwork.train(input, output)
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+        // Creates a M x 1 output matrix based on the pattern described above
+        var output = [[Double]](repeating: [Double](repeating: 0, count: 1), count: M)
+        for (rowIndex, row) in input.enumerated() {
+            for (index, value) in row.enumerated() {
+                if index == 0 && value == 1 && row[index + 1] == 0 {
+                    output[rowIndex] = [1]
+                    break
+                } else if index == 1 && value == 1 && row[index - 1] == 0 {
+                    output[rowIndex] = [1]
+                    break
+                }
+            }
+        }
+        
+        // Setup and train the neural network
+        let neuralNetwork = NeuralNetwork(activationFunction: .sigmoid, iterations: 10000, verbose: true)
+        let structure = neuralNetwork.train(input, output)
+        
+        // Calculate the number of unsure values. Values in the hidden layer that = 0.5 imply that the neural network wasn't sure of whether they should be a 0 or 1.
+        if let structure = structure {
+            print("\nNumber of unsure values (value = 0.5):")
+            print(structure.hiddenLayer.filter { $0 == 0.5 }.count)
         }
     }
-
-
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    
 }
 
